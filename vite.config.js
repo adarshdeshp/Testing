4f103c23940cadce7fd0cffaa63cf21d816f05a8
Adarsh Deshpande <addu9480@gmail.com>
2025-04-03 20:46:55 +0530

    Build production version and update vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/',
-    build: {
-        outDir: 'dist'
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
     }
+  },
+  server: {
+    port: 5173
+  }
 });
