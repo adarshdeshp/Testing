fa1ced03a8c679247eed09d1188c4b95a6cf5004
Adarsh Deshpande <addu9480@gmail.com>
2025-04-03 18:12:11 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './auth/login/LoginForm';
-// import Logout from './auth/logout/Logout';
+import Logout from './auth/logout/Logout';
 import SignupForm from './auth/signup/SignupForm';
 import PasswordResetForm from './auth/passwordReset/PasswordResetForm';
 
@@ -10,10 +10,9 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path="/login" element={<LoginForm />} />
-                {/* <Route path="/logout" element={<Logout />} /> */}
+                <Route path="/logout" element={<Logout />} />
                 <Route path="/signup" element={<SignupForm />} />
                 <Route path="/password-reset" element={<PasswordResetForm />} />
-                <Route path="/dashboard" element={<div>Dashboard (Protected Route)</div>} />
             </Routes>
         </Router>
     );
